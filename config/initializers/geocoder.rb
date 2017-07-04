976dffdcca092d542295b85c7da4a34605b59734
Salanoid <salanoid@gmail.com>
2017-07-04 10:33:54 +0000

    foo

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,12 +2,12 @@ Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
   lookup: :google,            # name of geocoding service (symbol)
-  ip_lookup: :ipapi_com,      # name of IP address geocoding service (symbol)
+  ip_lookup: :freegeoip,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  # api_key: AIzaSyA6nZL7hRFHgoBpO0rayEWSsSNeU2l8-QY,               # API key for geocoding service
+  # api_key: nil,               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
