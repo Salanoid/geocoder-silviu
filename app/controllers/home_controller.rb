59a16791c160425c29801fdfad2902122cb41894
Salanoid <salanoid@gmail.com>
2017-07-04 08:50:07 +0000

    foo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @user_location_country = request.location.country
     @user_location_city = request.location.city
+    @user_location_street = request.location.street
     @user_location_longitude = request.location.longitude
     @user_location_latitude = request.location.latitude
     @user_ip = request.remote_ip
