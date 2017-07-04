704a6c24a09ebdaabd1117cae8e4e2c2323dff58
Salanoid <salanoid@gmail.com>
2017-07-04 08:40:42 +0000

    foo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
-  geocoded_by :full_street_address 
-  after_validation :geocode 
 end
