abbb366f1ece33fd44c460dab5abc335f5ac560f
Salanoid <salanoid@gmail.com>
2017-07-04 12:09:29 +0000

    foo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,12 @@
 class HomeController < ApplicationController
   def index
+    @user_location_country = request.location.country
+    @user_location_city = request.location.city
     @user_location_longitude = request.location.longitude
     @user_location_latitude = request.location.latitude
     @user_ip = request.remote_ip
     @user_so = Geocoder.search("@user_ip")
   end
 end
+
+
